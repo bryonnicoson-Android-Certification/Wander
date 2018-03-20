de17db9c7234ec586ffec6b97bd90115a3c74e54
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-20 16:25:26 -0500

    Marker style

--- app/src/main/java/com/bryonnicoson/wander/MapsActivity.java
+++ app/src/main/java/com/bryonnicoson/wander/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -128,7 +129,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 map.addMarker(new MarkerOptions().position(latLng)
                         .title(getString(R.string.dropped_pin))
-                        .snippet(snippet));
+                        .snippet(snippet)
+                        .icon(BitmapDescriptorFactory.defaultMarker(
+                                BitmapDescriptorFactory.HUE_BLUE
+                        )));
             }
         });
     }
